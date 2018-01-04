65bfe03bc1d702e2503b84e789c9149dc71ce744
unknown <tr91kozy@studserv.uni-leipzig.de>
2018-01-04 15:58:57 +0100

    Sidescrolling funktioniert soweit boys!

--- main.h
+++ main.h
@@ -1,4 +1,4 @@
-#include "C:\Users\Tim Ruschke\Desktop\University\Prozedurale Programmierung\Project\Misc\include\SDL2\SDL.h"
+#include "C:\Users\Tim Ruschke\Desktop\University\Prozedurale Programmierung\Project\misc\include\SDL2\SDL.h"
 #include "C:\Users\Tim Ruschke\Desktop\University\Prozedurale Programmierung\Project\misc\include\SDL2\SDL_opengl.h"
 #include "C:\Users\Tim Ruschke\Desktop\University\Prozedurale Programmierung\Project\misc\include\SDL2\SDL_main.h"
 #include <stdbool.h>
@@ -18,6 +18,10 @@ typedef struct{
 } StaticObject;
 
 typedef struct{
+    float scrollX; // determines the part of the stage the hero/ player sees
+
+    StaticObject platforms[10];
+
     SDL_Renderer *renderer;
     Character hero;
     StaticObject ground[10]; 
