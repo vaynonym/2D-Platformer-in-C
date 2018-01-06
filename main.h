65612166d268aaa0285eb4910dce421110324723
unknown <tr91kozy@studserv.uni-leipzig.de>
2018-01-06 12:22:51 +0100

    hud almost working but currently commented out

--- main.h
+++ main.h
@@ -1,7 +1,9 @@
 #include "C:\Users\Tim Ruschke\Desktop\University\Prozedurale Programmierung\Project\misc\include\SDL2\SDL.h"
 #include "C:\Users\Tim Ruschke\Desktop\University\Prozedurale Programmierung\Project\misc\include\SDL2\SDL_opengl.h"
 #include "C:\Users\Tim Ruschke\Desktop\University\Prozedurale Programmierung\Project\misc\include\SDL2\SDL_main.h"
+#include "C:\Users\Tim Ruschke\Desktop\University\Prozedurale Programmierung\Project\misc\include\SDL2\SDL_ttf.h"
 #include <stdbool.h>
+
 
 
 typedef struct{
@@ -9,6 +11,7 @@ typedef struct{
     float dx, dy; // Current Velocity
     bool groundCollision; // Does the character currently touch the ground?
     char *name;
+    int lives;
     SDL_Texture *texture; // The texture used for 
 } Character;
 
@@ -20,11 +23,19 @@ typedef struct{
 typedef struct{
     float scrollX; // determines the part of the stage the hero/ player sees
 
-    StaticObject platforms[10];
-
     SDL_Renderer *renderer;
+
+    // Objects
     Character hero;
     StaticObject ground[10]; 
+    StaticObject platforms[40];
+
+    // Images
+    SDL_Texture *label;
+
+    // Fonts
+    TTF_Font *font;
+
 } GameState;
 
 bool processEvents(SDL_Window *window, GameState *game); // handles user input like closing the game, moving the hero etc.
