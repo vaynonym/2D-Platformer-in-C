3e3b4c766299354449e161dc3674b1cce9ef9b1f
unknown <tr91kozy@studserv.uni-leipzig.de>
2018-01-09 19:33:15 +0100

    Hud works now

--- init.c
+++ init.c
@@ -11,24 +11,57 @@
 void initHud(GameState *game){
     char livesStr[128] = "";
     sprintf(livesStr, "Lives: %d", game->hero.lives);
+    // char timeStr[128] = "";
+    // sprintf(timeStr, "%d", game->time);
+    game->currentStage = "Draftlevel 24601";
+
     SDL_Color white = {255,255,255,255};
 
-    SDL_Surface *tmpSur = TTF_RenderText_Blended(game->font, livesStr, white);
-    game->label = SDL_CreateTextureFromSurface(game->renderer, tmpSur);
-    SDL_FreeSurface(tmpSur);
+    SDL_Surface *tmpLives = TTF_RenderText_Blended(game->font, livesStr, white);
+    SDL_Surface *tmpStageName = TTF_RenderText_Blended(game->font, game->currentStage, white);
+    game->livesLabel = SDL_CreateTextureFromSurface(game->renderer, tmpLives);
+    game->stageLabel = SDL_CreateTextureFromSurface(game->renderer, tmpStageName);
+    SDL_FreeSurface(tmpLives);
+    SDL_FreeSurface(tmpStageName);
+
+
 }
 
 void drawHud(GameState *game){
+    SDL_Color white = {255,255,255,255};
+
+    // Makes a Texture for Time, dynamically updated every 60 frames. Corresponds roughly with time in seconds if run at 60 fps
+    if (game->time%60 == 0){
+        char timeStr[128] = "";
+        sprintf(timeStr, "%d", game->time / 60);
+        SDL_Surface *tmpTime = TTF_RenderText_Blended(game->font, timeStr, white);
+        game->timeLabel = SDL_CreateTextureFromSurface(game->renderer, tmpTime);
+        SDL_FreeSurface(tmpTime);
+    }
+    
     // draw in white
     SDL_SetRenderDrawColor(game->renderer, 255, 255, 255, 255);
     int w, h; //texture width and height
-    SDL_QueryTexture(game->label, NULL, NULL, &w, &h);
-    SDL_Rect textRect = {40 + game->scrollX , 40, w, h};
+    // livesLabel
+    SDL_QueryTexture(game->livesLabel, NULL, NULL, &w, &h);
+    SDL_Rect livesRect = {20, 20, w, h};
+    SDL_RenderCopy(game->renderer, game->livesLabel, NULL, &livesRect);
+    // timeLabel
+    SDL_QueryTexture(game->timeLabel, NULL, NULL, &w, &h);
+    SDL_Rect timeRect = {640, 20, w, h};
+    SDL_RenderCopy(game->renderer, game->timeLabel, NULL, &timeRect);
+    // stageName
+    SDL_QueryTexture(game->stageLabel, NULL, NULL, &w, &h);
+    SDL_Rect stageRect = {300, 20, w, h};
+    SDL_RenderCopy(game->renderer, game->stageLabel, NULL, &stageRect);
 
 }
 
 void clearHud(GameState *game){
-    if(game->label != NULL){
-        SDL_DestroyTexture(game->label);
+    if(game->livesLabel != NULL){
+        SDL_DestroyTexture(game->livesLabel);
+    }
+    if(game->timeLabel != NULL){
+        SDL_DestroyTexture(game->timeLabel);
     }
 }
