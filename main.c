2cc1fce987ef26608006fda621b76a0f12620403
unknown <tr91kozy@studserv.uni-leipzig.de>
2018-01-10 16:24:50 +0100

    jumping and running feels better now

--- main.c
+++ main.c
@@ -8,12 +8,12 @@
 #include "C:\Users\Tim Ruschke\Desktop\University\Prozedurale Programmierung\Project\misc\include\SDL2\SDL_ttf.h"
 #include "main.h"
 #include "init.h"
-#define GRAVITY 0.16f
+#define GRAVITY 0.32f
 
 
 // Resolution of the game
-const int width = 800; 
-const int height = 600;
+const int width = 1920; 
+const int height = 1080;
 const int levelWidth = 3000;
 
 void initGame(GameState *game, SDL_Window *gameWindow){
@@ -21,7 +21,7 @@ void initGame(GameState *game, SDL_Window *gameWindow){
     SDL_RENDERER_ACCELERATED  = the renderer uses hardware acceleration
     SDL_RENDERER_PRESENTVSYNC =  synchronized with the refresh rate of the monitor */
     game->renderer = SDL_CreateRenderer(gameWindow, // name of window
-                                                -1, // index of rendering driver, -1 being the first that supports the "requested flags"
+                                                -1, // index of rendering driver, -1 being the first that supports the requested flags
                                                 SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC // requested flags, explained above
                                                 );
     // hero
@@ -92,12 +92,12 @@ bool processEvents(SDL_Window *window, GameState *game){
     const Uint8 *state = SDL_GetKeyboardState(NULL);
     if(state[SDL_SCANCODE_LEFT]){
         if(game->hero.dx > -6){ // if the playerCharacter has not exceeded the maximum (negative for left) velocity, increase the current velocity
-            game->hero.dx -= 0.5;
+            game->hero.dx -= 2;
         }
     }
     if(state[SDL_SCANCODE_RIGHT]){
         if(game->hero.dx < 6){ // if the playerCharacter has not exceeded the maximum (positive for right) velocity, increase the current velocity
-            game->hero.dx += 0.5;
+            game->hero.dx += 2;
         }
     }
     if(game->hero.groundCollision && state[SDL_SCANCODE_UP]){
@@ -107,8 +107,8 @@ bool processEvents(SDL_Window *window, GameState *game){
     // NOTE: groundcollision has to be set on true when the hero touches the ground
     if(game->hero.maxdy < 0){ 
         if(state[SDL_SCANCODE_UP] && game->hero.jumping){ // The hero jumps when the user presses up
-            game->hero.dy -= 0.5;
-            game->hero.maxdy += 0.5; // NOTE: maxdy has to be set on normal when groundcollision becomes true
+            game->hero.dy -= 2;
+            game->hero.maxdy += 1.8; // NOTE: maxdy has to be set on normal when groundcollision becomes true
         }else{
             game->hero.jumping = false;
         }
@@ -128,7 +128,7 @@ void doRender(GameState *game){
 
     SDL_SetRenderDrawColor(game->renderer, 0, 255, 0, 255);
     for(int i = 0; i < 10; i++){
-        if(game->platforms[i].x + game->scrollX - game->platforms[i].width <= 800){ // only draw platforms which are visible on the screen
+        if(game->platforms[i].x + game->scrollX - game->platforms[i].width <= width){ // only draw platforms which are visible on the screen
             SDL_Rect platform = {game->platforms[i].x + game->scrollX, game->platforms[i].y, game->platforms[i].width, game->platforms[i].height};
             SDL_RenderFillRect(game->renderer, &platform);
             // Adding game->scrollX to each x-coordinate accomplishes the sidescrolling effect
@@ -192,7 +192,7 @@ int main(int argc, char* args[]){
                                               SDL_WINDOWPOS_CENTERED, // initial x position
                                               SDL_WINDOWPOS_CENTERED, // initial y position
                                               width, height, //each in pixels
-                                              SDL_WINDOW_OPENGL //flags, see https://wiki.libsdl.org/SDL_CreateWindow for a list of the flags
+                                              SDL_WINDOW_OPENGL | SDL_WINDOW_FULLSCREEN //flags, see https://wiki.libsdl.org/SDL_CreateWindow for a list of the flags
                                               );
     
     initGame(&game, gameWindow);
@@ -202,7 +202,13 @@ int main(int argc, char* args[]){
         if( !(game.hero.x < 0 && game.hero.dx < 0) && !(game.scrollX < -levelWidth && game.hero.dx > 0)){ // if the player is not trying to leave the screen
             game.hero.x += game.hero.dx; // adjust position of characters according to velocity
         }
+        // Physics
         game.hero.dy += GRAVITY;
+        const Uint8 *state = SDL_GetKeyboardState(NULL);
+        if(game.hero.dx > 0 && !state[SDL_SCANCODE_RIGHT])
+            game.hero.dx -= 2;  
+        else if(game.hero.dx < 0 && !state[SDL_SCANCODE_LEFT])
+            game.hero.dx += 2; 
         game.hero.y += game.hero.dy; //for now, the player can leave the screen vertically
 
         game.scrollX = -game.hero.x + width / 2; // the hero is always at the center of the screen (horizontally)
