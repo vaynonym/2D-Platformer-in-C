4b7af07fb597cfb977eec37dcfe94605a786eb4f
unknown <tr91kozy@studserv.uni-leipzig.de>
2018-01-28 11:56:35 +0100

    namechange and stuff

---
+++ hud.h
@@ -0,0 +1,15 @@
+#ifndef HUD_H
+#define HUD_H
+#include "main.h"
+
+
+
+// prototypes, allow calling from outside of main
+
+void initHud(GameState *game);
+
+void drawHud(GameState *game);
+
+void clearHud(GameState *game);
+
+#endif
